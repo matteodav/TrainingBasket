09874648bc550bc624868d621bb55c32c437abd1
Davi Matteo - Technogym SPA <mdavi@technogym.com>
2018-08-01 10:31:22 +0200

    Aggiungere i file di progetto.

---
+++ Technogym.MwCloud.Training.Basket/Classes/Configuration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OutOfWorkingHours.Training.Basket.Classes
+{
+
+    public class Configuration
+    {    
+        public Databaseconfiguration DatabaseConfiguration { get; set; }
+        public Smtpconfiguration SmtpConfiguration { get; set; }
+    }
+
+    public class Databaseconfiguration
+    {
+        public string ConnectionString { get; set; }
+    }
+
+    public class Smtpconfiguration
+    {
+        public string HostName { get; set; }
+        public string Port { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+
+}
---
+++ Technogym.MwCloud.Training.Basket/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OutOfWorkingHours.Training.Basket.Classes;
+
+namespace OutOfWorkingHours.Training.Basket
+{
+    public class Startup
+    {
+        private IHostingEnvironment env;
+
+
+        public Startup(IHostingEnvironment env)
+        {
+            this.env = env;
+        }
+
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        
+
+        public void Configure(IApplicationBuilder app)
+        {           
+            app.UseStaticFiles();
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(this.env.ContentRootPath, @"Configurations"))
+                .AddJsonFile(string.Concat(@"ConfigurationSettings.", this.env.EnvironmentName, @".json"))
+                .Build();
+
+            var config = new Configuration();
+            builder.Bind(config);
+            
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+
+
+        }
+    }
+}
